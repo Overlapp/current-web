d68305b458e98a73f97bc03b0e2e0849562935c2
Roeck <rons9n@gmail.com>
2024-07-17 15:00:32 -0500

    Video update on demo page

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -13,7 +13,7 @@ const Demo: NextPage = () => {
             <Meta title="Overlapp" />
             <PagesLayout>
                 <div className="demo">
-                    <VideoEmbed embedId="mtGPU6z-mCc" />
+                    <VideoEmbed embedId="S7ryxFtkyww" />
                     <iframe src="https://app.formbricks.com/s/clynoypwn000cy5gss0uhi1so" width="100%" height="500" style={{ border: "none" }} title="Contact Form"></iframe>
                 </div>
             </PagesLayout>
