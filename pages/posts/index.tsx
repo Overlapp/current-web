a216e62bfdd68a0efffd159caa36e8348216a960
Roeck <rons9n@gmail.com>
2024-07-26 18:07:02 -0500

    Styles update on blogs page

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -16,25 +16,17 @@ const Posts: NextPage = ({ posts }: any) => {
             <PagesLayout>
                 <div className="w-full bg-neutral-10 dark:bg-primary-pressed pb-80">
                     <div className="max-w-[1440px] mx-auto py-10 px-6 md:px-24">
-                        <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 items-start md:items-center justify-between mb-5">
-                            <div className="">
+                        <div className="flex flex-col md:flex-row space-y-3 md:space-y-0 items-start md:items-center md:text-size-10 justify-between mb-5">
                                 <PageText isLight>Blog Posts</PageText>
                                 <br></br>
                                 <ParagraphText className="text-3xl">Get to Know Overlapp!</ParagraphText>
                                 <ParagraphText className="text-3xl">
                                     Stay updated on how we're transforming the way you manage fun and connect with others. Discover our latest features and behind-the-scenes stories.
                                 </ParagraphText>
-                            </div>
                         </div>
-                        <div className="blogs md:flex-row flex flex-col ">
+                        <div className="blogs md:flex-1 md:grid md:gap-4 md:grid-cols-3 md:grid-rows-3 flex flex-col">
                             {posts.map((post: any, idx: number) => (
-                                <PostCard
-                                    key={idx}
-                                    imagePath={post.frontMatter.imageUrl}
-                                    postId={post.postId}
-                                    title={post.frontMatter.title}
-                                    category={post.frontMatter.category}
-                                />
+                                <PostCard key={idx} imagePath={post.frontMatter.imageUrl} postId={post.postId} title={post.frontMatter.title} category={post.frontMatter.category} />
                             ))}
                         </div>
                     </div>
@@ -69,4 +61,6 @@ export async function getStaticProps() {
 }
 
 export default Posts
+
+
 
