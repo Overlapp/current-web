f7c0df1b8eb5a4f4d5bb25d1ec0202dd1c169aee
Roeck <rons9n@gmail.com>
2024-07-26 15:07:44 -0500

    Share buttons

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from "next"
 import VideoEmbed from "../apps/components/VideoEmbed"
 import { Meta } from "@modules/Meta"
 import PagesLayout from "@layouts/PagesLayout"
+import ShareButtons from "@components/ShareButtons"
 import Footer from "@components/organism/Footer"
 
 const Demo: NextPage = () => {
@@ -14,12 +15,15 @@ const Demo: NextPage = () => {
                     <div className="video-container-demo">
                         <VideoEmbed embedId="S7ryxFtkyww" />
                     </div>
-                    <p className="demo-text md:pt-60 text-primary-pressed dark:text-neutral-10 font-gilroy-bold text-center mr-4 pt-6 pb-6 text-2xl">
+                    <p className="demo-text md:pt-60  text-primary-pressed dark:text-neutral-10 font-gilroy-bold text-center mr-4 pt-6 pb-6 text-2xl">
                         Thanks for watching our demo!<br></br>
-                        We'd love to hear your thoughts
-                        and feedback to make Overlapp
-                        even better.
+                        We'd love to hear your thoughts and feedback to make Overlapp even better.
+                        <br></br><br></br><br></br>
+                        Feel free to share!
                     </p>
+                    <div className="md:text-center">
+                        <ShareButtons />
+                    </div>
                     <div className="formbrick">
                         <iframe src="https://app.formbricks.com/s/clynoypwn000cy5gss0uhi1so" width="100%" height="500" style={{ border: "none" }} title="Contact Form"></iframe>
                     </div>
