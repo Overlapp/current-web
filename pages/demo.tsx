d7aef99a387f1444d63be04504e7ac0f013acfb0
Roeck <rons9n@gmail.com>
2024-07-29 17:34:08 -0500

    FormBricks height fix

--- pages/demo.tsx
+++ pages/demo.tsx
@@ -26,7 +26,7 @@ const Demo: NextPage = () => {
                         <ShareButtons />
                     </div>
                     <div className="formbrick">
-                        <iframe src="https://app.formbricks.com/s/clynoypwn000cy5gss0uhi1so" width="100%" height="500" style={{ border: "none" }} title="Contact Form"></iframe>
+                        <iframe src="https://app.formbricks.com/s/clynoypwn000cy5gss0uhi1so" width="100%" height="300" style={{ border: "none" }} title="Contact Form"></iframe>
                     </div>
                 </div>
             </PagesLayout>
