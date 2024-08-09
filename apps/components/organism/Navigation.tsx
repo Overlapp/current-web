b8f1c5ce7fd044676283e6b94a3e0372f7ece0b7
Roeck <rons9n@gmail.com>
2024-08-08 20:42:31 -0400

    Replaced burger policies for toggle button

--- apps/components/organism/Navigation.tsx
+++ apps/components/organism/Navigation.tsx
@@ -15,7 +15,7 @@ function Navigation() {
 
     return (
         <>
-            <nav className="bg-neutral-10 dark:bg-primary-pressed w-100">
+            <nav className="bg-neutral-10 dark:bg-primary-pressed w-100 pt-16">
                 <div className="md:px-24 md:w-full">
                     <div className="navbar">
                         <div className="w-full flex items-center justify-between">
@@ -29,71 +29,69 @@ function Navigation() {
                                     <div className="ml-16 md:ml-20 flex items-center space-x-10 relative z-20">
                                         <div className="section">
                                             <div>
-                                                <div className="footer__socials">
-                                                    <div className="main-links">
-                                                        <Link id="blog-link" href="/posts">
-                                                            <motion.p
-                                                                initial={{ opacity: 0, translateY: -40 }}
-                                                                animate={{ opacity: 1, translateY: 0 }}
-                                                                transition={{ duration: 0.4 }}
-                                                                className="nav-link"
-                                                                id="nav-1"
-                                                            >
-                                                                Blog
-                                                            </motion.p>
-                                                        </Link>
-                                                        <Link id="demo-link" href="/demo">
-                                                            <motion.p
-                                                                initial={{ opacity: 0, translateY: -40 }}
-                                                                animate={{ opacity: 1, translateY: 0 }}
-                                                                transition={{ duration: 0.4 }}
-                                                                className="nav-link"
-                                                                id="nav-1"
-                                                            >
-                                                                Demo
-                                                            </motion.p>
-                                                        </Link>
-                                                    </div>
-                                                    <Link href="https://app.termly.io/document/privacy-policy/d1aba773-0491-446b-b739-f3af98f29836">
+                                                <div className="main-links">
+                                                    <Link id="blog-link" href="/posts">
                                                         <motion.p
                                                             initial={{ opacity: 0, translateY: -40 }}
                                                             animate={{ opacity: 1, translateY: 0 }}
                                                             transition={{ duration: 0.4 }}
                                                             className="nav-link"
-                                                            id="nav-3"
+                                                            id="nav-1"
                                                         >
-                                                            Privacy
+                                                            Blog
                                                         </motion.p>
                                                     </Link>
-                                                    <Link href="https://app.termly.io/policy-viewer/policy.html?policyUUID=38a8003a-2bb3-40f9-a150-f89f2a4fbf87">
+                                                    <Link id="demo-link" href="/demo">
                                                         <motion.p
                                                             initial={{ opacity: 0, translateY: -40 }}
                                                             animate={{ opacity: 1, translateY: 0 }}
                                                             transition={{ duration: 0.4 }}
                                                             className="nav-link"
-                                                            id="nav-4"
+                                                            id="nav-1"
                                                         >
-                                                            Terms of Use
-                                                        </motion.p>
-                                                    </Link>
-                                                    <Link href="https://app.termly.io/document/cookie-policy/fb2341c1-d489-4e3c-a7cc-6a733f9144c4">
-                                                        <motion.p
-                                                            initial={{ opacity: 0, translateY: -40 }}
-                                                            animate={{ opacity: 1, translateY: 0 }}
-                                                            transition={{ duration: 0.4 }}
-                                                            className="nav-link"
-                                                            id="nav-5"
-                                                        >
-                                                            Cookies
+                                                            Demo
                                                         </motion.p>
                                                     </Link>
                                                 </div>
+                                                <Link href="https://app.termly.io/document/privacy-policy/d1aba773-0491-446b-b739-f3af98f29836">
+                                                    <motion.p
+                                                        initial={{ opacity: 0, translateY: -40 }}
+                                                        animate={{ opacity: 1, translateY: 0 }}
+                                                        transition={{ duration: 0.4 }}
+                                                        className="nav-link"
+                                                        id="nav-3"
+                                                    >
+                                                        Privacy
+                                                    </motion.p>
+                                                </Link>
+                                                <Link href="https://app.termly.io/policy-viewer/policy.html?policyUUID=38a8003a-2bb3-40f9-a150-f89f2a4fbf87">
+                                                    <motion.p
+                                                        initial={{ opacity: 0, translateY: -40 }}
+                                                        animate={{ opacity: 1, translateY: 0 }}
+                                                        transition={{ duration: 0.4 }}
+                                                        className="nav-link"
+                                                        id="nav-4"
+                                                    >
+                                                        Terms of Use
+                                                    </motion.p>
+                                                </Link>
+                                                <Link href="https://app.termly.io/document/cookie-policy/fb2341c1-d489-4e3c-a7cc-6a733f9144c4">
+                                                    <motion.p
+                                                        initial={{ opacity: 0, translateY: -40 }}
+                                                        animate={{ opacity: 1, translateY: 0 }}
+                                                        transition={{ duration: 0.4 }}
+                                                        className="nav-link"
+                                                        id="nav-5"
+                                                    >
+                                                        Cookies
+                                                    </motion.p>
+                                                </Link>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                            <div className="md:inline flex pr-[150px] md:pr-[20px] pt-10 py-[18px] ">
+                            <div className="md:inline flex md:pt-[30px] md:pr-[20px] hidden">
                                 <Switcher />
                             </div>
                             <div className="navigation-content">
@@ -107,20 +105,18 @@ function Navigation() {
                                     />
                                 </div>
                                 <div className="nav-container">
-                                    <p className="-mt-20 font-gilroy-bold text-center italic md:text-left pt-56 text-3xl md:text-7xl">
-                                        <Link href="/demo">DEMO</Link>
+                                    <p className="-mt-26 font-gilroy-bold text-center italic md:text-left pt-56 text-3xl md:text-7xl">
+                                        <Link href="/demo" className="flex pt-4 pl-36">
+                                            DEMO
+                                        </Link>
+                                        <Link href="/posts" className="flex pt-14 pl-36">
+                                            BLOG
+                                        </Link>
                                         <br></br>
                                         <br></br>
-                                        <Link href="/posts">BLOG</Link>
-                                        <br></br>
-                                        <br></br>
-                                        <Link href="https://app.termly.io/policy-viewer/policy.html?policyUUID=d1aba773-0491-446b-b739-f3af98f29836">PRIVACY</Link>
-                                        <br></br>
-                                        <br></br>
-                                        <Link href="https://app.termly.io/policy-viewer/policy.html?policyUUID=38a8003a-2bb3-40f9-a150-f89f2a4fbf87">TERMS OF USE</Link>
-                                        <br></br>
-                                        <br></br>
-                                        <Link href="https://app.termly.io/document/cookie-policy/fb2341c1-d489-4e3c-a7cc-6a733f9144c4">COOKIES</Link>
+                                        <div className="pl-44">
+                                            <Switcher />
+                                        </div>
                                     </p>
                                 </div>
                             </div>
@@ -133,6 +129,10 @@ function Navigation() {
 }
 
 export default Navigation
+
+
+
+
 
 
 
